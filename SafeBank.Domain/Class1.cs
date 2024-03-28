e092f5fc3628d14db3d0fe4943aea0bbb8b8c580
Grigoriy Plotnikov <plotnikovg17@gmail.com>
2024-03-28 17:19:39 +0300

    Added "domain" class library

---
+++ SafeBank.Domain/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace SafeBank.Domain;
+
+public class Class1
+{
+}
--- SafeBank.Domain/obj/Debug/net8.0/SafeBank.Domain.AssemblyInfo.cs
+++ SafeBank.Domain/obj/Debug/net8.0/SafeBank.Domain.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("SafeBank.API")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("SafeBank.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+551a4a0afae90bfb9d638c6900ce6a5880dc1f5a")]
-[assembly: System.Reflection.AssemblyProductAttribute("SafeBank.API")]
-[assembly: System.Reflection.AssemblyTitleAttribute("SafeBank.API")]
+[assembly: System.Reflection.AssemblyProductAttribute("SafeBank.Domain")]
+[assembly: System.Reflection.AssemblyTitleAttribute("SafeBank.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
