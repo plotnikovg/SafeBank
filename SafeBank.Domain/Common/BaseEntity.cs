a7bea7398131ce6d6895e91bd22a0448f2d5a6a6
Grigoriy Plotnikov <plotnikovg17@gmail.com>
2024-04-01 13:37:19 +0300

    Added BaseEntity logic

--- SafeBank.Domain/Common/BaseEntity.cs
+++ SafeBank.Domain/Common/BaseEntity.cs
@@ -4,5 +4,18 @@ public abstract class BaseEntity
 {
     public int Id { get; set; }
 
-	private List<BaseEvent> _domainEvents = new();
+    private List<BaseEvent> _domainEvents = new();
+
+    public void AddDomainEvent(BaseEvent event)
+    {
+        _domainEvents.Add(event);
+    }
+    public void RemoveDomainEvent(BaseEvent event)
+    {
+        _domainEvents.Remove(event);
+    }
+    public void ClearDomainEvent(BaseEvent event)
+    {
+        _domainEvents.Clear();
+    }
 }
